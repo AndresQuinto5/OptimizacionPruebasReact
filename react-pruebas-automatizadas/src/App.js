21115c19583b828baacc50284729c56a3314475f
Andres <qui18288@uvg.edu.gt>
2023-01-24 20:58:02 -0600

    creacion del navbar para utilizarlo

---
+++ react-pruebas-automatizadas/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import  './components/Navbar.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div>
+        // Tu contenido aquí
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
