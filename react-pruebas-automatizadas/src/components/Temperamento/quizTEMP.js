47dc48c7c320a48ae671a7fef2c16f9e9e35cfdd
AbrilPal <abrilpal99@gmail.com>
2023-01-30 17:32:37 -0600

    Formulario

--- react-pruebas-automatizadas/src/components/Temperamento/quizTEMP.js
+++ react-pruebas-automatizadas/src/components/Temperamento/quizTEMP.js
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { Formik, Field, Form } from 'formik';
 import Question from "./QuestionTEMP";
 import { QuizContext } from "../../contexts/quizTemperamento";
 import "./quiz.css";
@@ -29,20 +30,57 @@ const Quiz = () => {
       )}
       {!quizState.showResults && (
         <div>
-          <div className="score">
-            Question {quizState.currentQuestionIndex + 1}/
-            {quizState.questions.length}
-          </div>
-          <Question />
-          {quizState.currentAnswer && (
-            <div
-              onClick={() => dispatch({ type: "NEXT_QUESTION" })}
-              className="next-button"
-            >
-              Next question
-            </div>
-          )}
+          <p>
+            Por favor llenar los datos del formulario para poder tomar la prueba de temperamento.
+          </p>
+          <br></br>
+          <Formik
+            initialValues={{
+              nombre: '',
+              apellido: '',
+              dpi: '',
+            }}
+            onSubmit={async (values) => {
+              console.log(values)
+              await new Promise((r) => setTimeout(r, 500));
+              alert("Tiene que llenar todo el formulario");
+            }}
+          >
+            <Form>
+              <label htmlFor="nombre">Nombre</label>
+              <br></br>
+              <Field id="nombre" name="nombre" />
+              <br></br>
+              <br></br>
+              <label htmlFor="apellido">Apellido</label>
+              <br></br>
+              <Field id="apellido" name="apellido" />
+              <br></br>
+              <br></br>
+              <label htmlFor="dpi">DPI</label>
+              <br></br>
+              <Field id="dpi" name="dpi" maxLength="13" />
+              <br></br>
+              <br></br>
+              <button type="submit" className="next-button">Comenzar prueba</button>
+            </Form>
+          </Formik>
         </div>
+        // <div>
+        //   <div className="score">
+        //     Question {quizState.currentQuestionIndex + 1}/
+        //     {quizState.questions.length}
+        //   </div>
+        //   <Question />
+        //   {quizState.currentAnswer && (
+        //     <div
+        //       onClick={() => dispatch({ type: "NEXT_QUESTION" })}
+        //       className="next-button"
+        //     >
+        //       Next question
+        //     </div>
+        //   )}
+        // </div>
       )}
     </div>
   );
