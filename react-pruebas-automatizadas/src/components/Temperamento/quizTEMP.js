cf7e0dab68e0d6a2c59c756b77e0955be7a77647
AbrilPal <abrilpal99@gmail.com>
2023-01-30 18:06:39 -0600

    estados de formulario

--- react-pruebas-automatizadas/src/components/Temperamento/quizTEMP.js
+++ react-pruebas-automatizadas/src/components/Temperamento/quizTEMP.js
@@ -3,10 +3,12 @@ import { Formik, Field, Form } from 'formik';
 import Question from "./QuestionTEMP";
 import { QuizContext } from "../../contexts/quizTemperamento";
 import "./quiz.css";
+import React, { useState } from 'react';
 
 
 const Quiz = () => {
   const [quizState, dispatch] = useContext(QuizContext);
+  const [complete, setComplete] = useState(false);
 
   return (
     <div className="quiz">
@@ -28,60 +30,73 @@ const Quiz = () => {
           </div>
         </div>
       )}
-      {!quizState.showResults && (
+      {!quizState.showResults ?
         <div>
-          <p>
-            Por favor llenar los datos del formulario para poder tomar la prueba de temperamento.
-          </p>
-          <br></br>
-          <Formik
-            initialValues={{
-              nombre: '',
-              apellido: '',
-              dpi: '',
-            }}
-            onSubmit={async (values) => {
-              console.log(values)
-              await new Promise((r) => setTimeout(r, 500));
-              alert("Tiene que llenar todo el formulario");
-            }}
-          >
-            <Form>
-              <label htmlFor="nombre">Nombre</label>
+          {!complete ? 
+            <div>
+              <p>
+                Por favor llenar los datos del formulario para poder tomar la prueba de temperamento.
+              </p>
               <br></br>
-              <Field id="nombre" name="nombre" />
-              <br></br>
-              <br></br>
-              <label htmlFor="apellido">Apellido</label>
-              <br></br>
-              <Field id="apellido" name="apellido" />
-              <br></br>
-              <br></br>
-              <label htmlFor="dpi">DPI</label>
-              <br></br>
-              <Field id="dpi" name="dpi" maxLength="13" />
-              <br></br>
-              <br></br>
-              <button type="submit" className="next-button">Comenzar prueba</button>
-            </Form>
-          </Formik>
+              <Formik
+                initialValues={{
+                  nombre: '',
+                  apellido: '',
+                  dpi: '',
+                }}
+                onSubmit={async (values) => {
+                  console.log(values)
+                  if (values.apellido == "" || values.nombre == "" || values.dpi == ""){
+                    alert("Tiene que llenar todo el formulario");
+                    setComplete(false)
+                  }else{
+                    // aqui va para que guarde la info del usuario
+                    setComplete(true)
+                  }
+                }}
+              >
+                <Form>
+                  <label htmlFor="nombre">Nombre</label>
+                  <br></br>
+                  <Field id="nombre" name="nombre" />
+                  <br></br>
+                  <br></br>
+                  <label htmlFor="apellido">Apellido</label>
+                  <br></br>
+                  <Field id="apellido" name="apellido" />
+                  <br></br>
+                  <br></br>
+                  <label htmlFor="dpi">DPI</label>
+                  <br></br>
+                  <Field id="dpi" name="dpi" maxLength="13" />
+                  <br></br>
+                  <br></br>
+                  <button type="submit" className="next-button">Comenzar prueba</button>
+                </Form>
+              </Formik>
+            </div>
+          :
+          <div>
+            <div>
+              <div className="score">
+             Question {quizState.currentQuestionIndex + 1}/
+             {quizState.questions.length}
+            </div>
+             <Question />
+            {quizState.currentAnswer && (
+              <div
+                 onClick={() => dispatch({ type: "NEXT_QUESTION" })}
+                   className="next-button"
+                 >
+                   Next question
+                 </div>
+              )}
+           </div></div>}
         </div>
-        // <div>
-        //   <div className="score">
-        //     Question {quizState.currentQuestionIndex + 1}/
-        //     {quizState.questions.length}
-        //   </div>
-        //   <Question />
-        //   {quizState.currentAnswer && (
-        //     <div
-        //       onClick={() => dispatch({ type: "NEXT_QUESTION" })}
-        //       className="next-button"
-        //     >
-        //       Next question
-        //     </div>
-        //   )}
-        // </div>
-      )}
+      :
+        <div></div>
+      }
+      
     </div>
   );
 };
