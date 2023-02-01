da6e880e995755e7a281d637ec6601378599ae2b
AbrilPal <abrilpal99@gmail.com>
2023-01-31 20:18:32 -0600

    nueva logica de calificacion con Map

--- react-pruebas-automatizadas/src/contexts/quizTemperamento.js
+++ react-pruebas-automatizadas/src/contexts/quizTemperamento.js
@@ -12,7 +12,8 @@ const initialState = {
   sangineoAnswers: 0,
   colericoAnswers: 0,
   melancolicoAnswers: 0,
-  flematicoAnswers: 0
+  flematicoAnswers: 0,
+  mapa: new Map()
 };
 
 const generateAnswers = (question) => {
@@ -42,15 +43,17 @@ const reducer = (state, action) => {
     }
     case "NEXT_QUESTION": {
       let currentAnswer = state.currentAnswer;
+      console.log(state.currentQuestionIndex, "esto es el index de la pregunta?")
       if(currentAnswer == 1){
-        state.sangineoAnswers = state.sangineoAnswers + 1
+        state.mapa.set(state.currentQuestionIndex, currentAnswer)
       }else if (currentAnswer == 2){
-        state.colericoAnswers = state.colericoAnswers + 1
+        state.mapa.set(state.currentQuestionIndex, currentAnswer)
       }else if (currentAnswer == 3){
-        state.melancolicoAnswers = state.melancolicoAnswers + 1
+        state.mapa.set(state.currentQuestionIndex, currentAnswer)
       }else if (currentAnswer == 4){
-        state.flematicoAnswers = state.flematicoAnswers + 1
+        state.mapa.set(state.currentQuestionIndex, currentAnswer)
       }
+      console.log()
       const showResults =
         state.currentQuestionIndex === state.questions.length - 1;
       const currentQuestionIndex = showResults
@@ -59,6 +62,21 @@ const reducer = (state, action) => {
       const answers = showResults
         ? []
         : AnswerList(state.questions[currentQuestionIndex]);
+
+      if(showResults === true){
+        console.log(state.mapa)
+        state.mapa.forEach((valor,clave)=> {
+          if(valor == 1){
+            state.sangineoAnswers += 1
+          }else if(valor == 2){
+            state.colericoAnswers += 1
+          }else if(valor == 3){
+            state.melancolicoAnswers += 1
+          }else if(valor == 4){
+            state.flematicoAnswers += 1
+          }
+        })
+      }
       return {
         ...state,
         currentAnswer: "",
