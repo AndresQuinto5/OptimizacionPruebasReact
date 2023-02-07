387c17edf32314c5c81d26e0605349ddb7b4714e
Andres <qui18288@uvg.edu.gt>
2023-02-07 13:16:07 -0600

    images on questions

--- react-pruebas-automatizadas/src/components/TIE/QuestionTIE.js
+++ react-pruebas-automatizadas/src/components/TIE/QuestionTIE.js
@@ -1,6 +1,10 @@
 import { useContext } from "react";
 import { QuizContextTIE } from "../../contexts/quizTIE";
 import AnswerTIE from "./AnswerTIE";
+import imagen1 from "../../assets/Picture1.png";
+import imagen2 from "../../assets/Picture2.png";
+import imagen3 from "../../assets/Picture3.png";
+import imagen4 from "../../assets/Picture4.png";
 
 const QuestionTIE = () => {
   const [quizState, dispatch] = useContext(QuizContextTIE);
@@ -8,6 +12,10 @@ const QuestionTIE = () => {
   return (
     <div>
       <div className="question">{currentQuestion.question}</div>
+      <div className="hola">{quizState.currentQuestionIndex === 19 && <img src={imagen1}/>}
+      {quizState.currentQuestionIndex === 37 && <img src={imagen2}/>}
+      {quizState.currentQuestionIndex === 55 && <img src={imagen3}/>}
+      {quizState.currentQuestionIndex === 73 && <img src={imagen4}/>}</div>
       <div className="answers">
         {quizState.answers.map((answer, index) => (
           <AnswerTIE
