c5029f1d506bdc9b3ad83afb330b6b7ca1a23faf
Andres <qui18288@uvg.edu.gt>
2023-03-02 10:19:43 -0600

    elimine la funcion de enviar email al actualizar datos del form de home

--- react-pruebas-automatizadas/src/components/pruebas/home.js
+++ react-pruebas-automatizadas/src/components/pruebas/home.js
@@ -82,8 +82,6 @@ return (
                     handleMergeArrays();
                     handleSetState(true);
                     navigate('/Temperamento');
-
-                    sendEmail();
                   }
                 }}
               >
